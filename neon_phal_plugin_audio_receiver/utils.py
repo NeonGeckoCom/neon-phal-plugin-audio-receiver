aa841378c0f439013c8c1a907b80db3b4555078a
Mike <mike@graywind.org>
2023-08-15 22:36:11 -0500

    bluetooth fix

--- neon_phal_plugin_audio_receiver/utils.py
+++ neon_phal_plugin_audio_receiver/utils.py
@@ -177,7 +177,7 @@ def set_uxplay_device_name(name: str, service_file_path: Optional[str] = None) -
 
 
 def auto_pair_bluetooth(timeout: int = 60) -> None:
-    subprocess.run(f"/usr/local/bin/autopair-bluetooth.sh {timeout}", check=True)
+    subprocess.run(f"sudo /usr/local/bin/autopair-bluetooth.sh {timeout}", check=True)
 
 
 def auto_pair_kdeconnect(timeout: int = 30) -> None:
