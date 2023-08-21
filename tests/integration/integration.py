d2b8a5c5bee9e9561e14b9468a1390df8c9c69f9
Daniel McKnight <daniel@neongecko.com>
2023-08-21 15:00:31 -0700

    Mark integration test as failing for GHA compat.

--- tests/integration/integration.py
+++ tests/integration/integration.py
@@ -1,3 +1,5 @@
+import pytest
+
 # from neon_phal_plugin_audio_receiver.utils import auto_pair_bluetooth, auto_pair_kdeconnect
 # from neon_phal_plugin_audio_receiver.utils import set_raspotify_device_name
 # from neon_phal_plugin_audio_receiver.utils import set_uxplay_device_name
@@ -8,15 +10,18 @@ from neon_phal_plugin_audio_receiver.utils import interact_with_service, get_ser
 # set_raspotify_device_name("Neon Mark 2")
 # set_uxplay_device_name("Neon Mark 2")
 
-# - Get the status of any of these services on the Mark 2
-services = ("uxplay", "raspotify", "bluetooth", "kdeconnect")
-for service in services:
-    print(f"{service} is running? {get_service_status(service)}")
 
-# - Enable/disable the services
-for service in services:
-    # You should see "True" for all of these messages
-    interact_with_service(service, "stop")
-    print(f"{service} stopped, did it register? {not get_service_status(service)}")
-    interact_with_service(service, "start")
-    print(f"{service} started, did it register? {get_service_status(service)}")
+@pytest.mark.xfail
+def test_service_enable():
+    # - Get the status of any of these services on the Mark 2
+    services = ("uxplay", "raspotify", "bluetooth", "kdeconnect")
+    for service in services:
+        print(f"{service} is running? {get_service_status(service)}")
+
+    # - Enable/disable the services
+    for service in services:
+        # You should see "True" for all of these messages
+        interact_with_service(service, "stop")
+        print(f"{service} stopped, did it register? {not get_service_status(service)}")
+        interact_with_service(service, "start")
+        print(f"{service} started, did it register? {get_service_status(service)}")
